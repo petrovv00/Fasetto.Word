8fb153bf128332dbf4f266b8d004d1944ec2a38b
urospetrov <uros.petrov12@gmail.com>
2023-10-11 14:37:54 +0200

    Initial changes commited, fonts imported.

---
+++ Fasetto.Word/ViewModel/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasetto.Word.ViewModel
+{
+    /// <summary>
+    /// A base view model that fires Property changed events as needed
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
